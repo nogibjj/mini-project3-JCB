64f76f00cec4fe9f37a1362036d445605f94f5e8
cervs257 <cervantes257@gmail.com>
2023-09-13 17:48:26 -0400

    modified download to melt the dataframe and write long csv

--- download_currencies.py
+++ download_currencies.py
@@ -11,3 +11,13 @@ def download_prices(tickers):
 def create_tickers(ccy_list):
     tickers = [create_ticker(ticker) for ticker in ccy_list]
     return tickers
+
+
+def download_prices_long(tickers):
+    data = yf.download(tickers, period="1mo", interval="60m")
+    data.reset_index(inplace=True)
+    data.melt(
+        id_vars="Datetime", var_name=["Price type", "Instrument"], value_name="Price"
+    )
+    data.to_csv("currency_prices_long.csv")
+    pass
